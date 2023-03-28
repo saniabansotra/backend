157218b78f7ff603d5f671e08734a4a8a059552f
Sania Bansotra <saniabansotra@gmail.com>
2023-03-28 20:33:29 +0530

    message

--- connection/index4.js
+++ connection/index4.js
@@ -1,18 +1,43 @@
+// const express = require("express");
+// const mongoose = require("mongoose"); // works as middleware b/w database and server
+// const app = express(); //top most line of server
+// const { connectDatabase } = require("./cile");
+
+// // const connectDatabase = () => {
+// //   try {
+// //     mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
+// //       // useFindAndModify: true,
+// //       console.log("database connected");
+// //     });
+// //   } catch (error) {
+// //     console.log(error);
+// //   }
+// // };
+
+// connectDatabase();
+// const PORT = 8000;
+
+// app.listen(PORT, () => {
+//   // bottom most line
+//   console.log("Server is running on port ", PORT);
+// });
+
 const express = require("express");
 const mongoose = require("mongoose"); // works as middleware b/w database and server
 const app = express(); //top most line of server
 const { connectDatabase } = require("./cile");
 
-// const connectDatabase = () => {
-//   try {
-//     mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
-//       // useFindAndModify: true,
-//       console.log("database connected");
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+//main code
+app.use(express.json());
+app.post("/savedata", (req, res) => {
+  try {
+    console.log(req.body);
+    return res.json({ success: true, message: "data connected" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, error: error.message });
+  }
+});
 
 connectDatabase();
 const PORT = 8000;
