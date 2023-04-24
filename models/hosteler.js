aff062361b0125a30bebf6045b3d14d73cba2345
Sania Bansotra <saniabansotra@gmail.com>
2023-04-24 12:39:56 +0530

    message

--- models/hosteler.js
+++ models/hosteler.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-const hosteler_schema = new mongoose.Schema({
-  h_name: String,
-  h_mother_name: String,
-  h_father_name: String,
-  h_number: Number,
-  h_roomNo: Number,
-});
+const hosteler_schema = new mongoose.Schema(
+  {
+    h_name: String,
+    h_mother_name: String,
+    h_father_name: String,
+    h_number: Number,
+    h_roomNo: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 const hosteler = mongoose.model("hosteler", hosteler_schema);
 module.exports = hosteler;
